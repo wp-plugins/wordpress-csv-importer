feb1e60217399b35623c339e55c890fabfff4934
WebTechGlobal <WebTechGlobal@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-15 03:06:25 +0000

    Adding 0.2.1 Tag
    git-svn-id: https://plugins.svn.wordpress.org/wordpress-csv-importer/tags/0.2.1@572379 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templatesystem/wtgcsv_load_admin_includes_templatesystem.php
@@ -0,0 +1,7 @@
+<?php
+require(WTG_CSV_DIR.'templatesystem/include/wtgcsv_admininterface_functions.php');
+require(WTG_CSV_DIR.'templatesystem/include/wtgcsv_ajax_admin_functions.php');  
+require(WTG_CSV_DIR.'templatesystem/include/wtgcsv_install_functions.php');
+require(WTG_CSV_DIR.'templatesystem/include/wtgcsv_admin_functions.php');
+require(WTG_CSV_DIR.'templatesystem/include/wtgcsv_sql_functions.php');       
+?>
